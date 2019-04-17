95c8a1ce323163e6badcdf8ebfbd71e21f168e80
ahmedgedi <ahmed.gedi1@gmail.com>
2019-04-17 14:36:29 -0700

    get requests work

--- src/App.js
+++ src/App.js
@@ -4,49 +4,68 @@ import moment from 'moment';
 import axios from 'axios';
 import './App.css';
 
-const url = 'http://50.17.105.33/api/v0/all';
+const url = 'http://34.220.171.44/locations.json';
+
+
+// const reformatStops = function(stops) {
+//   var info = 
+  
+//   return info;
+// }
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       date: moment(),
-      location: ['Templeton', 'Shattuck Hall', 'Fred Meyer']
+      location: []
     }
   }
 
-  componentWillMount() {
-   
+  componentWillMount() { 
     axios.get(url)
-    .then(response => console.log(response.data)); 
-  
-    alert("this has ended");
+    .then(response => 
+      this.setState({location: response.data}));
     
   }
+
+
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h3>PioTracker 2.0</h3>
+          <h4> <br/> {this.state.date.format("LTS")}</h4>
         </header>
         <body>
           <Container>
             <div className="arrivalTimes">
              {/* Just some hardcoded example times, hopefully we can mess around with the moment library and Pio Location  */}
               <p>
-                The bus will arrive at {this.state.location[2]} {this.state.date.subtract(10, "minutes").toNow() }
+                The bus will arrive at {this.state.location[0]} 
               </p>
 
             </div>
           </Container>
           <Container>
             <div className="arrivalTimes">
-              <p>The bus will arrive at {this.state.location[0]} {this.state.date.subtract(10, "minutes").toNow() }</p>
+              <p>The bus will arrive at {this.state.location[1]}</p>
             </div>
           </Container>
           <Container>
             <div className="arrivalTimes">
-              <p>The bus will arrive at {this.state.location[1]} {this.state.date.add(15, "minutes").toNow() }</p>
+              <p>The bus will arrive at {this.state.location[2]} </p>
+            </div>
+          </Container>
+          <Container>
+            <div className="arrivalTimes">
+              <p>The bus will arrive at {this.state.location[3]}</p>
+            </div>
+          </Container>
+          <Container>
+            <div className="arrivalTimes">
+              <p>The bus will arrive at {this.state.location[4]}</p>
             </div>
           </Container>
         </body>

d28159399e52ee7c2c6e344bbdd0b01e63b12ea6
ahmedgedi <ahmed.gedi1@gmail.com>
2019-04-19 16:04:10 -0700

    functional http requests now

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import React, { Component } from 'react';
-import {Container} from 'react-bootstrap';
+import {Container, Row} from 'react-bootstrap';
 import moment from 'moment';
 import axios from 'axios';
 import './App.css';
 
-const url = 'http://34.220.171.44/locations.json';
+const url = 'http://174.129.44.93/api/v0/all';
 
-
-// const reformatStops = function(stops) {
-//   var info = 
-  
-//   return info;
-// }
 
 class App extends Component {
   constructor(props) {
@@ -22,12 +16,34 @@ class App extends Component {
     }
   }
 
+
   componentWillMount() { 
+
+   let addStops = (response) => { 
+      var data = [];
+      for(var i = 0; i < 6; i++) {
+        data[i] = response.data[i].formatted;
+      }
+      this.setState({location: data});
+      return data;
+    }
+   
     axios.get(url)
-    .then(response => 
-      this.setState({location: response.data}));
+    .then(function (response) {
+      addStops(response);
+      
+      
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+
     
-  }
+}
+
+
+
+  
 
 
 
@@ -35,39 +51,45 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <h3>PioTracker 2.0</h3>
-          <h4> <br/> {this.state.date.format("LTS")}</h4>
+        <Container>
+          <Row><h1>PioTracker 2.0 </h1></Row>
+          <Row><em>Never miss the Pio again</em></Row>
+          <Row>
+            <h3> {this.state.date.format("LTS")}</h3>
+          </Row>
+        </Container>
         </header>
         <body>
           <Container>
             <div className="arrivalTimes">
              {/* Just some hardcoded example times, hopefully we can mess around with the moment library and Pio Location  */}
               <p>
-                The bus will arrive at {this.state.location[0]} 
+                Bus 1 is currently at {this.state.location[0]} 
               </p>
 
             </div>
           </Container>
           <Container>
             <div className="arrivalTimes">
-              <p>The bus will arrive at {this.state.location[1]}</p>
+              <p>Bus 2 is currently at {this.state.location[1]}</p>
             </div>
           </Container>
           <Container>
             <div className="arrivalTimes">
-              <p>The bus will arrive at {this.state.location[2]} </p>
+              <p>Bus 3 is currently at {this.state.location[2]} </p>
             </div>
           </Container>
           <Container>
             <div className="arrivalTimes">
-              <p>The bus will arrive at {this.state.location[3]}</p>
+              <p>Bus 4 is currently at {this.state.location[3]}</p>
             </div>
           </Container>
           <Container>
             <div className="arrivalTimes">
-              <p>The bus will arrive at {this.state.location[4]}</p>
+              <p>Bus 5 is currently at {this.state.location[4]}</p>
             </div>
           </Container>
+
         </body>
       </div>
     );
